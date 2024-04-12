75d3376b418a5f4bf3b3d87ec594209ecbf343b3
CYNTHIA_ONYINYECHI <cynthiadamian22@gmail.com>
2024-04-13 00:35:51 +0100

    added some functionality to the buttons

--- FreeCodeCamp/script.js
+++ FreeCodeCamp/script.js
@@ -97,7 +97,7 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-    if (currentWeapon < weapons.length ) {
+    if (currentWeapon < weapons.length -1) {
         if (gold >= 30) {
             gold -= 30;
             currentWeapon += 1;
@@ -109,6 +109,10 @@ function buyWeapon() {
         } else {
             text.innerText = "You do not have enough gold to buy a weapon.";
         }
+    } else {
+        text.innerText = "You already have the most powerful weapon!";
+        button2.innerText = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon();
     }
     
 };
