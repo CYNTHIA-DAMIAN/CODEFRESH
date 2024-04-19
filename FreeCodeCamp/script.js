593c1ff8ece0fe73fd7510c02645cbd531dffb52
CYNTHIA-DAMIAN <cythianduka97@gmail.com>
2024-04-19 01:03:29 +0100

    All functional-RPG game is complete!

--- FreeCodeCamp/script.js
+++ FreeCodeCamp/script.js
@@ -1,3 +1,4 @@
+//Line 2-18 for all the variables in a global scope
 let xp = 0;
 let health = 100;
 let gold = 50;
@@ -82,8 +83,8 @@ const locations = [
       },
       {
         name: "kill monster",
-        "button text": ["Go to town square", "Go to town square", "Go to town square"],
-        "button functions": [goTown, goTown, goTown],
+        "button text": ["Go to town square", "Go to town square", "Go to easterEgg"],
+        "button functions": [goTown, goTown, easterEgg],
         text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
       },
       {
@@ -97,6 +98,12 @@ const locations = [
         "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
         "button functions": [restart, restart, restart],
         text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;"
+      },
+      { 
+        name: "easter egg", 
+        "button text": ["2", "8", "Go to town square?"], 
+        "button functions": [pickTwo, pickEight, goTown], 
+         text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
       }
 ];
 
@@ -113,7 +120,7 @@ function update(location) {
     button1.onclick = location["button functions"][0];
     button2.onclick = location["button functions"][1];
     button3.onclick = location["button functions"][2];
-    text.innerHTML = location.text= "You are in the town square. You see a sign that says \"Store\".";
+    text.innerHTML = location.text;
 };
 
 function goTown() {
@@ -198,6 +205,7 @@ function  sellWeapon() {
 function attack(){
   text.innerText = "The " + monsters[fighting].name + " attacks."
   text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+  text.innerText += " Your " + inventory.pop() +  " breaks."
   health -= getMonsterAttackValue(monsters[fighting].level);
   if (isMonsterHit()){
     monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1 ; 
@@ -215,6 +223,9 @@ function attack(){
   } else {
     defeatMonster();
   };
+  if (Math.random() <= .1 && inventory.length !== 1){
+    currentWeapon--
+}
 };
 
 function getMonsterAttackValue(level){
@@ -222,6 +233,10 @@ function getMonsterAttackValue(level){
     console.log(hit);
     return hit > 0 ? hit : 0; // removed & replaced this "return hit" bcos of the negative value it returned at some point;
 };
+
+function isMonsterHit(){
+    return Math.random() > .2 || health < 20;
+  };
 
 function dodge(){
  text.innerText = "You dodge the attack from the " + monsters[fighting].name;
@@ -255,8 +270,39 @@ function restart() {
     goTown();
   }
 
+  function easterEgg(){
+    update(locations[7])
+  };
 
+  function pickTwo(){
+    pick(2)
+  };
+  
+  function pickEight(){
+    pick(8);
+  };
 
+  function pick(guess){
+    const numbers = [];
+    while (numbers.length < 10) {
+       numbers.push(Math.floor(Math.random() * 11))
+    }
+    text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+    for (let i = 0; i < 10; i++) {
+       text.innerText += numbers[i] + "\n"
+    }
+    if (numbers.includes(guess)) {
+        text.innerText += "Right! You win 20 gold!";
+        gold += 20;
+        goldText.innerText = gold;
+    } else {
+        text.innerText += "Wrong! You lose 10 health!";
+        health -= 10;
+        healthText.innerText = health;
+      } if (health <= 0){
+        lose();
+      }
+};  
 
 
 
